57098d23ee32c16b3c3f4d159de38498e536b007
Sergey Plashenko <spl@sitecore.net>
2018-08-21 11:40:11 +0300

    Config with all needed connection strings was added

---
+++ src/Sitecore.Support.249748/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.249748")]
+[assembly: AssemblyProduct("Sitecore.Support.249748")]
+[assembly: ComVisible(false)]
